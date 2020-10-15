318c9ff58482715b034bec4a43605662817089bc
khotigor <khotigor177@gmail.com>
2020-10-16 00:29:48 +0300

    init commit, 2 View added

---
+++ LiqLiq/LiqLiq/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiqLiq
+//
+//  Created by Igor Khotyanovich on 15.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
